c5aac886560b72a07eb688f73a278140fc89c9a5
hiperreal <abihudcastrillo@gmail.com>
2026-05-31 22:17:38 -0600

    búsqueda de productos funcionando

--- src/app/pages/client/home/home.page.ts
+++ src/app/pages/client/home/home.page.ts
@@ -20,6 +20,7 @@ export class HomePage implements OnInit, OnDestroy {
   sectionTitle: string = 'Lo más pedido';
   cartCount: number = 0;
   userRole: string = '';
+  searchQuery: string = '';
   private sub!: Subscription;
 
   constructor(
@@ -32,21 +33,14 @@ export class HomePage implements OnInit, OnDestroy {
     this.sub = this.cartService.cart$.subscribe(
       (items) => { this.cartCount = items.length; }
     );
-
     const auth = getAuth();
     onAuthStateChanged(auth, async (user) => {
-      console.log('USUARIO ACTUAL:', user);
       if (user) {
         const db = getFirestore();
         const snap = await getDoc(doc(db, 'users', user.uid));
         if (snap.exists()) {
           this.userRole = (snap.data() as any)['role'];
-          console.log('ROL DEL USUARIO:', this.userRole);
-        } else {
-          console.log('NO SE ENCONTRÓ EL DOCUMENTO DEL USUARIO');
         }
-      } else {
-        console.log('NO HAY USUARIO AUTENTICADO');
       }
     });
   }
@@ -55,8 +49,22 @@ export class HomePage implements OnInit, OnDestroy {
     if (this.sub) this.sub.unsubscribe();
   }
 
+  onSearch(event: any): void {
+    const query = event.target.value.toLowerCase().trim();
+    this.searchQuery = query;
+    if (!query) {
+      this.filterCat(this.activeCat);
+      return;
+    }
+    this.filteredItems = this.allItems.filter(i =>
+      i.name.toLowerCase().includes(query)
+    );
+    this.sectionTitle = 'Resultados de búsqueda';
+  }
+
   filterCat(cat: string): void {
     this.activeCat = cat;
+    this.searchQuery = '';
     this.sectionTitle = cat === 'Todos' ? 'Lo más pedido' : cat;
     this.filteredItems = cat === 'Todos'
       ? [...this.allItems]
@@ -67,15 +75,7 @@ export class HomePage implements OnInit, OnDestroy {
     this.router.navigate(['/client/detail', item.id]);
   }
 
-  goToCart(): void {
-    this.router.navigate(['/client/cart']);
-  }
-
-  goToKds(): void {
-    this.router.navigate(['/restaurant/kds']);
-  }
-
-  goToOrders(): void {
-    this.router.navigate(['/client/orders']);
-  }
+  goToCart(): void { this.router.navigate(['/client/cart']); }
+  goToKds(): void  { this.router.navigate(['/restaurant/kds']); }
+  goToOrders(): void { this.router.navigate(['/client/orders']); }
 }
